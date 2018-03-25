959565943b971ff2a2d3b5d68a1a43d3b15b27ec
yvasilyev92 <yvasilye@mail.sfsu.edu>
2018-03-25 13:30:10 -0700

    Initial Commit

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,80 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    Request[] public requests;
+    uint public approversCount;
+
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        if(!approvers[msg.sender]){
+            approvers[msg.sender] = true;
+            approversCount++;
+        }
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
+        });
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage currentRequest = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!currentRequest.approvals[msg.sender]);
+
+        currentRequest.approvals[msg.sender] = true;
+        currentRequest.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage currentRequest = requests[index];
+
+        require(currentRequest.approvalCount > (approversCount / 2));
+        require(!currentRequest.complete);
+
+        currentRequest.recipient.transfer(currentRequest.value); 
+        currentRequest.complete = true;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+}
