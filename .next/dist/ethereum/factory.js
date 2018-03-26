1527a21290173a242d4f0bb764c45ce651ca88b3
yvasilyev92 <yvasilye@mail.sfsu.edu>
2018-03-25 19:37:49 -0700

    Updated index.js with getInitialProps

---
+++ .next/dist/ethereum/factory.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _web = require('./web3');
+
+var _web2 = _interopRequireDefault(_web);
+
+var _CampaignFactory = require('./build/CampaignFactory.json');
+
+var _CampaignFactory2 = _interopRequireDefault(_CampaignFactory);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Pass in Contract ABI & Address of already deployed contract.
+// Tell web3 that a deployed copy of CampaignFactory exists.
+var instance = new _web2.default.eth.Contract(JSON.parse(_CampaignFactory2.default.interface), '0x3229bf26668B483fAB9fe2fd731Bf2f194C67642');
+
+exports.default = instance;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImV0aGVyZXVtXFxmYWN0b3J5LmpzIl0sIm5hbWVzIjpbIndlYjMiLCJDYW1wYWlnbkZhY3RvcnkiLCJpbnN0YW5jZSIsImV0aCIsIkNvbnRyYWN0IiwiSlNPTiIsInBhcnNlIiwiaW50ZXJmYWNlIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFDQSxBQUFPLEFBQVU7Ozs7QUFDakIsQUFBTyxBQUFxQjs7Ozs7O0FBRTVCO0FBSkE7QUFLQSxJQUFNLFdBQVcsSUFBSSxjQUFBLEFBQUssSUFBVCxBQUFhLFNBQzVCLEtBQUEsQUFBSyxNQUFNLDBCQURJLEFBQ2YsQUFBMkIsWUFEN0IsQUFBaUIsQUFFZixBQUdGOztrQkFBQSxBQUFlIiwiZmlsZSI6ImZhY3RvcnkuanMiLCJzb3VyY2VSb290IjoiQzovVXNlcnMvWWV2Z2VuaXkvRGVza3RvcC9ldGhlcl9wcmFjdGljZS9raWNrc3RhcnQifQ==
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
+import factory from '../ethereum/factory.js';
 
-export default () => {
-  return (
-    <h1>This is the campaign list page!</h1>
-  );
-};
+class CampaignIndex extends Component {
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+    return { campaigns };
+  }
+
+  render() {
+    return <div>{this.props.campaigns[0]}</div>
+  }
+}
+
+export default CampaignIndex;
