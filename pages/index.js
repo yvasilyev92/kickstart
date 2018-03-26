3d4dc0e305c4616d58b7b3b26eaa7b4715299d6a
yvasilyev92 <yvasilye@mail.sfsu.edu>
2018-03-25 23:01:32 -0700

    Added open-campaigns header

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ class CampaignIndex extends Component {
   render() {
     return (<div>
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+        <h3>Open Campaigns</h3>
         {this.renderCampaigns()}
         <Button content="Create Campaign" icon="add circle" primary={true}/>
     </div>);
