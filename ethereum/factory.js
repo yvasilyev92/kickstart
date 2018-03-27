3d5f417c774ee9e0eb8277de9d49aa739017200d
yvasilyev92 <yvasilye@mail.sfsu.edu>
2018-03-26 19:25:21 -0700

    Redeployed Updated Contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -5,7 +5,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 // Pass in Contract ABI & Address of already deployed contract.
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x3229bf26668B483fAB9fe2fd731Bf2f194C67642'
+  '0x4831c8AbB48208385d5f82Db8abE39a3839cC643'
 );
 
 export default instance;
