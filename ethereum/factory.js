1f7d99b04f6eddc1669fea8d47591ee1deaa6ed0
yvasilyev92 <yvasilye@mail.sfsu.edu>
2018-04-01 13:11:23 -0700

    Updated address for factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -5,7 +5,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 // Pass in Contract ABI & Address of already deployed contract.
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x4831c8AbB48208385d5f82Db8abE39a3839cC643'
+  ' 0xaA843b9fef2Db21c27b5EB810844d32267A3510E'
 );
 
 export default instance;
