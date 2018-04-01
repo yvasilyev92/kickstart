194be1b360281aa7168638be69160c317f4e3c91
yvasilyev92 <yvasilye@mail.sfsu.edu>
2018-04-01 13:09:52 -0700

    Recompiled new contract & updated gas limit

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -13,7 +13,7 @@ const deploy = async () => {
 
   const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({data: compiledFactory.bytecode})
-    .send({gas: '1000000', from: accounts[0]});
+    .send({gas: '2000000', from: accounts[0]});
 
   console.log('Contract deployed to: ', result.options.address);
 };
